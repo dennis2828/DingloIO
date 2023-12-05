e1da5253453eafc4f014f2c2978f056378ba28f0
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-12-05 21:33:26 +0200

    feat - create account

--- app/(routes)/account/components/forms/create-account-form.tsx
+++ app/(routes)/account/components/forms/create-account-form.tsx
@@ -53,7 +53,6 @@ export const CreateAccountForm = () =>{
             <h1 className="font-bold text-[1.15em] xss:text-[1.19em] xs:text-[1.25em] xsMd:text-[1.4em] xsBig:text-[1.45em] sm:tet-[1.5em] md:text-[2em] lg:text-[2.5em]">Create an account</h1>
             <p>Authenticate and unleash the power of <span className="font-bold">Dinglo.<span className="text-softBlue">IO</span></span></p>
             <form className="mt-4" onSubmit={handleSubmit((data)=>{
-                console.log("subvmit", data);
                 createAccount(data);
             })}>
                 <div className="flex flex-col gap-2">
--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -1,33 +1,49 @@
 import db from "@/lib/db";
 import { AccountValidator } from "@/validators/account";
+import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { ZodError } from "zod";
 
-export async function POST(req: NextRequest){
-    try{
-        const res = await req.json();
-        const {password, confirmPassword, ...accountData} = AccountValidator.parse(res);
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const { password, confirmPassword, ...accountData } =
+      AccountValidator.parse(data);
 
-        if(password){
-            if(password.trim()!==confirmPassword.trim())
-                return new NextResponse("Password doesn't match",{status:400});
-        }
-
-        const newAccount = await db.user.create({
-            data:{
-                password,
-                ...accountData,
-            },
-        });
-
-        return NextResponse.json({account:newAccount, msg:"Your account was successfully created !"},{status:200});
-
-    }catch(error){
-        console.log();
-        
-        if(error instanceof ZodError)
-            return new NextResponse(error.issues[0].message,{status:500});
-
-        return new NextResponse("Something went wrong. Please try again later.",{status:500});
+    if (password) {
+      if (password.trim() !== confirmPassword.trim())
+        return new NextResponse("Password doesn't match", { status: 400 });
     }
+
+    const newAccount = await db.user.create({
+      data: {
+        password,
+        ...accountData,
+      },
+    });
+
+    return NextResponse.json(
+      { account: newAccount, msg: "Your account was successfully created !" },
+      { status: 200 }
+    );
+  } catch (error) {
+
+    if (error instanceof ZodError)
+      return new NextResponse(error.issues[0].message, { status: 500 });
+    
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        if (error.meta?.target) {
+          return new NextResponse(
+            //@ts-ignore
+            `This ${error.meta.target[0]} already exists`
+          ,{status:500});
+        }
+      }
+    }
+
+    return new NextResponse("Something went wrong. Please try again later.", {
+      status: 500,
+    });
+  }
 }
