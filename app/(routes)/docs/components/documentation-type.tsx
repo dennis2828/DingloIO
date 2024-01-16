126a5a42f3be0e12f3adef9c602b7d303226ed8f
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-01-16 20:21:11 +0200

    chore - fix documentation navigation

--- app/(routes)/docs/components/documentation-type.tsx
+++ app/(routes)/docs/components/documentation-type.tsx
@@ -7,8 +7,12 @@ import Link from "next/link";
 
 export const DocumentationType = ({documentation}:{documentation: DocumentationSection}) =>{
     const pathname = usePathname();
+
+    let pathnameRoute = pathname.split("/")[2] || "docs";
+
+    let path = documentation.path.split('/')[2] || "docs";
     
     return (
-        <Link href={documentation.path} className={cn(pathname.includes(documentation.path) ? "text-softBlue font-semibold":"text-black dark:text-gray-300 font-medium hover:text-gray-500 dark:hover:text-white", "cursor-pointer duration-100")}>{documentation.title}</Link>
+        <Link href={documentation.path} className={cn(pathnameRoute===path ? "text-softBlue font-semibold":"text-black dark:text-gray-300 font-medium hover:text-gray-500 dark:hover:text-white", "cursor-pointer duration-100")}>{documentation.title}</Link>
     )
 }
