da750c13fa8062e0a265f71dee066163c326e67e
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-01-16 20:37:17 +0200

    chore - fix docs page menu

--- app/(routes)/docs/components/documentation-type.tsx
+++ app/(routes)/docs/components/documentation-type.tsx
@@ -8,9 +8,21 @@ import Link from "next/link";
 export const DocumentationType = ({documentation}:{documentation: DocumentationSection}) =>{
     const pathname = usePathname();
 
-    let pathnameRoute = pathname.split("/")[2] || "docs";
+    let pathnameRoute = "/docs/"+pathname.split("/")[2] || "/docs";
 
-    let path = documentation.path.split('/')[2] || "docs";
+    let path;
+    if(pathname===documentation.path)
+        path=pathnameRoute;
+    else
+    for(const pathLink of documentation.links){
+        
+        for(const subLink of pathLink.links){
+            if(subLink.link===pathnameRoute)
+                path=subLink.link;
+        }
+    }
+    
+    console.log(path,pathnameRoute);
     
     return (
         <Link href={documentation.path} className={cn(pathnameRoute===path ? "text-softBlue font-semibold":"text-black dark:text-gray-300 font-medium hover:text-gray-500 dark:hover:text-white", "cursor-pointer duration-100")}>{documentation.title}</Link>
