937de8723271f2d2a45d921b3f1e0e0cb8bd74dd
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-01-22 18:59:36 +0200

    chore -  text experimental page

--- app/(routes)/experimental/page.tsx
+++ app/(routes)/experimental/page.tsx
@@ -9,7 +9,7 @@ const DashboardExperimentalPage = async () =>{
             <Container>
                 <h1 className="font-bold text-[1.5em] text-center">
                 This simulates a real scenario between a client and the admin. <br/>
-                Make sure that you have also the dashboard open. You should see a new connection instance
+                Make sure that you have also the dashboard open on the default_app project. You should see a new connection instance
                 </h1>
             </Container>
             <DingloWrapper/>
