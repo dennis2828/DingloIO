504b8c9f11b1e8bf5b145a89d1a51a126918d904
Dennis Moldovan <dennismoldovan32@gmail.com>
2025-05-01 21:20:45 +0300

    chore - add npm package

--- app/(routes)/components/Npm/npm-package.tsx
+++ app/(routes)/components/Npm/npm-package.tsx
@@ -7,10 +7,10 @@ import { Copy } from "lucide-react"
 export const NPMPackage = ()=>{
     return (
         <div onClick={()=>{
-            copyText("npm install dinglo-io")
+            copyText("npm install dinglo-io-chat")
             toast.success("Successfully copied");
         }} className="cursor-pointer font-roboto tracking-[6px] p-1 rounded-md flex items-center gap-2 text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-500 duration-150">
-            npm install dinglo-chat-widget
+            npm install dinglo-io-chat
             <div>
                 <Copy className="w-4 h-4"/>
             </div>
